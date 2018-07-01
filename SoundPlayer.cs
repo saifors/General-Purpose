62669893844ab8ccd1e40ba347c65fe624a573ee
Dion Schreuder <40234847+saifors@users.noreply.github.com>
2018-07-01 18:50:29 +0200

    Add files via upload

---
+++ SoundPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    public AudioClip[] clips;
+	
+    public void PlaySound(int num, float vol, bool is2D)
+    {
+        GameObject obj = new GameObject();
+        obj.transform.position = transform.position;
+        obj.name = "AUDIO_" + clips[num].name;
+
+        AudioSource source = obj.AddComponent<AudioSource>();
+        source.clip = clips[num];
+        source.volume = vol;
+        //source.pitch = pit; //Not necessary but can be optional.
+        if(is2D) source.spatialBlend = 0;
+        else source.spatialBlend = 0;
+        source.Play();
+
+        Destroy(obj, clips[num].length); //Don't mistake length(length of clip) with "L"ength (Length of array)
+    }
+    public void PlaySound(int num, float vol, bool is2D, float pit)
+    {
+        GameObject obj = new GameObject();
+        obj.transform.position = transform.position;
+        obj.name = "AUDIO_" + clips[num].name;
+
+        AudioSource source = obj.AddComponent<AudioSource>();
+        source.clip = clips[num];
+        source.volume = vol;
+        source.pitch = pit; //Not necessary but can be optional.
+        if (is2D) source.spatialBlend = 0;
+        else source.spatialBlend = 0;
+        source.Play();
+
+        Destroy(obj, clips[num].length);
+    }
+
+    //Parented to an object?
+
+    //PlayMusicLoop?
+}
