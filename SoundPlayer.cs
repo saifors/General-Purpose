431ee22bb30710a1bf14e83f3a0b178254099c1d
Dion Schreuder <40234847+saifors@users.noreply.github.com>
2018-07-01 19:31:27 +0200

    Update SoundPlayer.cs

--- SoundPlayer.cs
+++ SoundPlayer.cs
@@ -4,42 +4,76 @@ using UnityEngine;
 
 public class SoundPlayer : MonoBehaviour
 {
-    public AudioClip[] clips;
-	
+public AudioClip[] soundClips;
+    public AudioClip[] musicClips;
+
     public void PlaySound(int num, float vol, bool is2D)
     {
         GameObject obj = new GameObject();
         obj.transform.position = transform.position;
-        obj.name = "AUDIO_" + clips[num].name;
+        obj.name = "AUDIO_" + soundClips[num].name;
 
         AudioSource source = obj.AddComponent<AudioSource>();
-        source.clip = clips[num];
+        source.clip = soundClips[num];
         source.volume = vol;
         //source.pitch = pit; //Not necessary but can be optional.
         if(is2D) source.spatialBlend = 0;
-        else source.spatialBlend = 0;
+        else source.spatialBlend = 1;
         source.Play();
 
-        Destroy(obj, clips[num].length); //Don't mistake length(length of clip) with "L"ength (Length of array)
+        Destroy(obj, soundClips[num].length); //Don't mistake length(length of clip) with "L"ength (Length of array)
     }
     public void PlaySound(int num, float vol, bool is2D, float pit)
     {
         GameObject obj = new GameObject();
         obj.transform.position = transform.position;
-        obj.name = "AUDIO_" + clips[num].name;
+        obj.name = "AUDIO_" + soundClips[num].name;
 
         AudioSource source = obj.AddComponent<AudioSource>();
-        source.clip = clips[num];
+        source.clip = soundClips[num];
         source.volume = vol;
         source.pitch = pit; //Not necessary but can be optional.
         if (is2D) source.spatialBlend = 0;
-        else source.spatialBlend = 0;
+        else source.spatialBlend = 1;
         source.Play();
 
-        Destroy(obj, clips[num].length);
+        Destroy(obj, soundClips[num].length);
     }
 
     //Parented to an object?
+    public void PlaySound(int num, float vol, bool is2D, float pit, Transform parent)
+    {
+        GameObject obj = new GameObject();        
+        obj.transform.position = parent.transform.position;
+        obj.name = "AUDIO_" + soundClips[num].name;
+        obj.transform.SetParent(parent);
+
+
+        AudioSource source = obj.AddComponent<AudioSource>();
+        source.clip = soundClips[num];
+        source.volume = vol;
+        source.pitch = pit; //Not necessary but can be optional.
+        if (is2D) source.spatialBlend = 0;
+        else source.spatialBlend = 1;
+        source.Play();
+
+        Destroy(obj, soundClips[num].length); //Don't mistake length(length of clip) with "L"ength (Length of array)
+    }
 
     //PlayMusicLoop?
+    //UNFINISHED, TODO: Figure out how to loop.
+    public void PlayMusic(int num, float vol)
+    {
+        GameObject obj = new GameObject();
+        obj.transform.position = transform.position;
+        obj.name = "MUSIC_" + soundClips[num].name;
+
+        AudioSource source = obj.AddComponent<AudioSource>();
+        source.clip = soundClips[num];
+        source.volume = vol;
+        source.spatialBlend = 1;
+        source.Play();
+
+        Destroy(obj, soundClips[num].length);
+    }
 }
